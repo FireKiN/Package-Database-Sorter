36a8bd3ac1090ccd26e662bfadbc94b0f4dc9cf7
jay-moon <46448473+jay-moon@users.noreply.github.com>
2019-01-11 15:21:36 -0500

    unique ids and a few other small tweaks
    added unique id functionality to the program. required a new boolean so that it only checks for duplicate ids if the user hasnt entered valid info in the first place.
    basically the program just reads through data.txt to look for any matching ids from the one the user entered (these comparisons will work regardless of if the user has 0's in front of the id or not) and then if it matches it stops reading data.txt and tells the user they have entered a duplicate id and sets the isInformationCorrect boolean to false so that it doesnt add that duplicate id to data.txt.
    
    deleted some System.out.println lines that are not required. 
    
    there might have been some other little lines i deleted that had no use etc. however the program is now stable

--- applicationFunctions.java
+++ applicationFunctions.java
@@ -1,8 +1,8 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.text.DecimalFormat;
+import javax.swing.*;
 
 public class applicationFunctions implements ActionListener {
 
@@ -11,7 +11,9 @@ public class applicationFunctions implements ActionListener {
     static File databaseIDTracker = new File("src\\id.txt");
 
     public void actionPerformed(ActionEvent a) {
-        //The reason why I use the class name to locate the button instead of creating an object from the class can be located here: http://www.dgp.toronto.edu/~trendall/course/108/lectures/L03node2.html
+        // The reason why I use the class name to locate the button instead of
+        // creating an object from the class can be located here:
+        // http://www.dgp.toronto.edu/~trendall/course/108/lectures/L03node2.html
         if (a.getSource() == applicationBuild.btnView) {
             try {
                 File dataFile = new File("src//data.txt");
@@ -20,68 +22,109 @@ public class applicationFunctions implements ActionListener {
                 if (lineToCheck != null) {
                     databaseBuild.isThereOneEntry = true;
                 }
-            }catch (IOException error) {
+            } catch (IOException error) {
                 System.out.print("Error in view button");
             }
             if (databaseBuild.isThereOneEntry == false) {
-                JOptionPane.showMessageDialog(null, "You must enter at least one package", "Error", 0);
+                JOptionPane.showMessageDialog(null, "You must enter at least one package", "Invalid Package Entry", 3);
             } else {
                 new databaseBuild();
             }
-            System.out.println("View Button Clicked");
         }
 
         if (a.getSource() == applicationBuild.btnEnter) {
-            //This variable is going to be used to check if the information entered is correct. There is going to be an if statement checking
-            //to see if it is correct, and if it is, the program will proceed.
+            // This variable is going to be used to check if the information
+            // entered is correct. There is going to be an if statement checking
+            // to see if it is correct, and if it is, the program will proceed.
             boolean isInformationCorrect = true;
+            boolean noInvalidEntries = true;
             try {
                 packageID = Integer.parseInt(applicationBuild.txtPackageID.getText());
                 applicationBuild.txtPackageID.setBackground(Color.WHITE);
                 if (packageID < 0 || packageID > 99999999) {
-                    //The packageID needs to be an 8 digit number so if it is more than 8 digits or a number less than 0, the packageID is incorrect,
-                    //Rendering the information also incorrect hence the next boolean.
+                    // The packageID needs to be an 8 digit number so if it is
+                    // more than 8 digits or a number less than 0, the packageID
+                    // is incorrect,
+                    // Rendering the information also incorrect hence the next
+                    // boolean.
                     isInformationCorrect = false;
-                    //This exception is thrown to make sure that the catch statement is run because the JOptionPane needs to be ran and the background
-                    //needs to be changed to pink since the information is wrong.
+                    noInvalidEntries = false;
+                    // This exception is thrown to make sure that the catch
+                    // statement is run because the JOptionPane needs to be ran
+                    // and the background
+                    // needs to be changed to pink since the information is
+                    // wrong.
                     throw new NumberFormatException();
                 } else {
                     isInformationCorrect = true;
+                    noInvalidEntries = true;
                 }
             } catch (NumberFormatException error) {
                 isInformationCorrect = false;
                 applicationBuild.txtPackageID.setBackground(Color.PINK);
-                JOptionPane.showMessageDialog(null, "Please enter a valid package ID", "Error", 0);
+                JOptionPane.showMessageDialog(null, "Please enter a valid package ID", "Invalid PackageID", 3);
             }
 
             if (applicationBuild.txtPackageName.getText().equals("")) {
                 isInformationCorrect = false;
+                noInvalidEntries = false;
                 applicationBuild.txtPackageName.setBackground(Color.PINK);
-                JOptionPane.showMessageDialog(null, "Please enter a package name", "Error", 0);
+                JOptionPane.showMessageDialog(null, "Please enter a package name", "Invalid Package Name", 3);
             } else {
+                noInvalidEntries = true;
                 applicationBuild.txtPackageName.setBackground(Color.WHITE);
             }
 
-            if (isInformationCorrect == true)  {
+            DecimalFormat packageIDDF = new DecimalFormat("00000000");
+            String formattedPackageID = packageIDDF.format(packageID);
+
+            if(noInvalidEntries == true) {
+                // Try catch to check if the ID is already used in the textfile
+                // This was placed here because the program needs to obtain the
+                // formatted package ID
+                try {
+                    String[] currentLineComponents;
+                    BufferedReader br = new BufferedReader(new FileReader(dataFile));
+                    String currentLine = br.readLine();
+
+                    while (currentLine != null) {
+                        currentLineComponents = currentLine.split("<>");
+                        if (currentLineComponents[0].equals(formattedPackageID)) {
+                            isInformationCorrect = false;
+                            JOptionPane.showMessageDialog(null, "The following ID already exists!", "Existing ID", 3);
+                            break;
+                        } else {
+                            isInformationCorrect = true;
+                        }
+                        currentLine = br.readLine();
+                    }
+                    br.close();
+                } catch (IOException error) {
+                    System.out.println("Unique ID error");
+                }
+            }
+
+            if (isInformationCorrect == true) {
+                JOptionPane.showMessageDialog(null, "Success!", "Data Entered", 1);
                 databaseBuild.databaseIDNum++;
                 applicationBuild.lblDatabaseIDNum.setText(Integer.toString(databaseBuild.databaseIDNum));
                 databaseBuild.isThereOneEntry = true;
+                //DELETE LATER
+                System.out.println("Database ID: " + databaseBuild.databaseIDNum + " | Package ID: "
+                        + formattedPackageID + " Package Name: " + applicationBuild.txtPackageName.getText());
 
-                DecimalFormat packageIDDF = new DecimalFormat("00000000");
-                String formattedPackageID = packageIDDF.format(packageID);
+                try {
+                    BufferedWriter out = new BufferedWriter(new FileWriter(databaseIDTracker, false));
+                    out.write(Integer.toString(databaseBuild.databaseIDNum));
+                    out.close();
+                } catch (IOException error) {
+                    System.out.println("Could not create file");
+                }
 
-                System.out.println("Database ID: " + databaseBuild.databaseIDNum + " | Package ID: " + formattedPackageID + " Package Name: " + applicationBuild.txtPackageName.getText());
-                
                 try {
-					BufferedWriter out = new BufferedWriter(new FileWriter(databaseIDTracker, false));
-					out.write(Integer.toString(databaseBuild.databaseIDNum));
-					out.close();
-				} catch (IOException error) {
-					System.out.println("Could not create file");
-				}
-                
-                try {
-                    String[] informationTypes = {formattedPackageID, Integer.toString(databaseBuild.databaseIDNum), applicationBuild.txtPackageName.getText(), applicationBuild.formattedDate, applicationBuild.sm.getValue().toString()};
+                    String[] informationTypes = { formattedPackageID, Integer.toString(databaseBuild.databaseIDNum),
+                            applicationBuild.txtPackageName.getText(), applicationBuild.formattedDate,
+                            applicationBuild.sm.getValue().toString() };
                     BufferedWriter out = new BufferedWriter(new FileWriter(dataFile, true));
                     for (int i = 0; i < informationTypes.length; i++) {
                         out.write(informationTypes[i] + "<>");
@@ -103,51 +146,52 @@ public class applicationFunctions implements ActionListener {
         }
 
         if (a.getSource() == applicationBuild.btnDelete) {
-            String deleteID = JOptionPane.showInputDialog(null, "Enter the ID of the package you would like to delete", "Delete Package", 3);
+            String deleteID = JOptionPane.showInputDialog(null, "Enter the ID of the package you would like to delete",
+                    "Delete Package", 3);
             File tempFile = new File("src\\temp.txt");
             try {
                 String[] currentLineComponents;
-                BufferedReader bw = new BufferedReader(new FileReader(dataFile));
-                BufferedWriter bw2 = new BufferedWriter(new FileWriter(tempFile));
-                String currentLine = bw.readLine();
+                BufferedReader br = new BufferedReader(new FileReader(dataFile));
+                BufferedWriter bw = new BufferedWriter(new FileWriter(tempFile));
+                String currentLine = br.readLine();
                 boolean noIDFound = false;
 
                 while (currentLine != null) {
                     currentLineComponents = currentLine.split("<>");
                     if (!currentLineComponents[0].equals(deleteID)) {
-                        bw2.write(currentLine);
-                        bw2.newLine();
+                        bw.write(currentLine);
+                        bw.newLine();
                         noIDFound = false;
                     } else {
                         noIDFound = true;
                     }
-                    currentLine = bw.readLine();
+                    currentLine = br.readLine();
                 }
-                if(noIDFound == false) {
+                if (noIDFound == false) {
                     JOptionPane.showMessageDialog(null, "Success!", "Deletion Completed", 1);
                 } else {
-                    JOptionPane.showMessageDialog(null, "The selected ID was not found", "Deletion Failed", 1);
+                    JOptionPane.showMessageDialog(null, "The selected ID was not found", "Deletion Failed", 3);
                 }
                 System.out.println("Done searching");
+                br.close();
                 bw.close();
-                bw2.close();
             } catch (IOException error) {
                 System.out.print("Error with deleting.");
             }
             try {
                 dataFile.delete();
                 dataFile = new File("src\\data.txt");
-                BufferedReader bw = new BufferedReader(new FileReader(tempFile));
-                BufferedWriter bw2 = new BufferedWriter(new FileWriter(dataFile));
-                String currentLine = bw.readLine();
-                while(currentLine != null) {
-                    bw2.write(currentLine);
-                    bw2.newLine();
-                    currentLine = bw.readLine();
+                BufferedReader br = new BufferedReader(new FileReader(tempFile));
+                BufferedWriter bw = new BufferedWriter(new FileWriter(dataFile));
+                String currentLine = br.readLine();
+                while (currentLine != null) {
+                    bw.write(currentLine);
+                    bw.newLine();
+                    currentLine = br.readLine();
                 }
+                br.close();
                 bw.close();
-                bw2.close();
-            } catch(IOException error) {
+            } catch (IOException error) {
                 System.out.println("Error rewriting the file.");
             }
         }

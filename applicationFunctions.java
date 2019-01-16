f56645d9effe3423d3cdd1246f3dba8d4d41ed60
Ryan <sheppardryan@rogers.com>
2019-01-15 23:43:53 -0500

    Added Documentation

--- applicationFunctions.java
+++ applicationFunctions.java
@@ -17,55 +17,71 @@ public class applicationFunctions implements ActionListener {
 
     public void actionPerformed(ActionEvent a) {
 
-        // The reason why I use the class name to locate the button instead of creating an object from the class can be located here:
-        // http://www.dgp.toronto.edu/~trendall/course/108/lectures/L03node2.html
+        //The reason why we use the class name to locate the button instead of creating an object from the class can be located here:
+        //http://www.dgp.toronto.edu/~trendall/course/108/lectures/L03node2.html
+        //In basic terms, the reason why we use the class name to reference objects in that class is because you cannot use the
+        //conventional way that we were taught which was to create a new object from the class and reference all the variables of the
+        //class through that object. You can not reference an object's object, so this was a way to easily get past this problem.
+
+        //VIEW BUTTON
         if (a.getSource() == applicationBuild.btnView) {
             try {
                 File dataFile = new File("src//data.txt");
                 BufferedReader out = new BufferedReader(new FileReader(dataFile));
                 String lineToCheck = out.readLine();
+                //This checks to see if the line in the data.txt file is not empty. If it is empty that means that the file has no entries
+                //in it. The reason why we check if there are any entries is because we do not want the user clicking the view button to view
+                //the database when there are no entries in the table.
                 if (lineToCheck != null) {
                     databaseBuild.isThereOneEntry = true;
                 }
             } catch (IOException error) {
-                System.out.print("Error in view button");
+                System.out.println("Error in view button");
             }
+            //If there is no package, we want to tell the user to enter a new package through a JOptionPane.
             if (databaseBuild.isThereOneEntry == false) {
                 JOptionPane.showMessageDialog(null, "You must enter at least one package", "Invalid Package Entry", 3);
-            } else {
+            }
+            //If all is good and there is at least one entry, we want to display the databaseBuild.
+            else {
                 new databaseBuild();
             }
         }
 
+        //ENTER BUTTON
         if (a.getSource() == applicationBuild.btnEnter) {
             // This variable is going to be used to check if the information entered is correct. There is going to be an if statement checking to see if it is
             // correct, and if it is, the program will proceed.
-            boolean isInformationCorrect = true;
-            boolean noInvalidEntries = true;
+            boolean isInformationCorrect;
+            boolean noInvalidEntries;
             try {
+                //The reason why this is in a try block is because if the program tries to convert letters to an integer, this obviously wont work.
                 packageID = Integer.parseInt(applicationBuild.txtPackageID.getText());
+                //Sets the textbox to white in case it was previously pink from a previous error.
                 applicationBuild.txtPackageID.setBackground(Color.WHITE);
+
+                //Checks to see if the ID is greater than 8 digits or is a negative number
                 if (packageID < 0 || packageID > 99999999) {
-                    // The packageID needs to be an 8 digit number so if it is more than 8 digits or a number less than 0, the packageID is incorrect, Rendering
-                    // the information also incorrect hence the next boolean.
-                    isInformationCorrect = false;
-                    noInvalidEntries = false;
                     // This exception is thrown to make sure that the catch statement is run because the JOptionPane needs to be ran and the background needs to be
                     // changed to pink since the information is wrong.
                     throw new NumberFormatException();
                 } else {
+                    //This else statement only runs if the user has entered a package ID that contains no letters and if it is 8 digits or less therefore the
+                    //information is true hence the next variable.
                     isInformationCorrect = true;
-                    noInvalidEntries = true;
-                    if (applicationBuild.txtPackageName.getText().equals("")) {
-                        isInformationCorrect = false;
-                        noInvalidEntries = false;
-                        applicationBuild.txtPackageName.setBackground(Color.PINK);
-                        JOptionPane.showMessageDialog(null, "Please enter a package name", "Invalid Package Name", 3);
-                    } else {
-                        noInvalidEntries = true;
-                        applicationBuild.txtPackageName.setBackground(Color.WHITE);
-                    }
                 }
+                //Checks to see if the package name is empty.
+                if (applicationBuild.txtPackageName.getText().equals("")) {
+                    isInformationCorrect = false;
+                    noInvalidEntries = false;
+                    applicationBuild.txtPackageName.setBackground(Color.PINK);
+                    JOptionPane.showMessageDialog(null, "Please enter a package name", "Invalid Package Name", 3);
+                } else {
+                    noInvalidEntries = true;
+                    //Sets the package name textbox back to white in case it was pink from a previous error.
+                    applicationBuild.txtPackageName.setBackground(Color.WHITE);
+                }
+
             } catch (NumberFormatException error) {
                 isInformationCorrect = false;
                 noInvalidEntries = false;
@@ -73,21 +89,29 @@ public class applicationFunctions implements ActionListener {
                 JOptionPane.showMessageDialog(null, "Please enter a valid package ID", "Invalid PackageID", 3);
             }
 
+            //We format the package ID that the user entered so that it looks good in the databaseBuild table.
             DecimalFormat packageIDDF = new DecimalFormat("00000000");
             String formattedPackageID = packageIDDF.format(packageID);
 
             if (noInvalidEntries == true) {
                 // Try catch to check if the ID is already used in the textfile This was placed here because the program needs to obtain the formatted package ID
                 try {
+                    //currentLineComponents is going to store each parsed part of the line in the data.txt file.
                     String[] currentLineComponents;
                     BufferedReader br = new BufferedReader(new FileReader(dataFile));
+                    //The current line is going to be the line that we are using.
                     String currentLine = br.readLine();
 
                     while (currentLine != null) {
+                        //We split the current line by the string of text that separates each piece of information in the data.txt file
                         currentLineComponents = currentLine.split("<>");
+                        //We only want to be searching through the first instance which would be the package ID. This is why we only check currentLineComponents[0]
+                        //in this try catch block.
                         if (currentLineComponents[0].equals(formattedPackageID)) {
+                            //If the ID has been found in the text file, the information is no longer correct.
                             isInformationCorrect = false;
                             JOptionPane.showMessageDialog(null, "The following ID already exists!", "Existing ID", 3);
+                            //break because we do not need to search through the rest of the text file.
                             break;
                         } else {
                             isInformationCorrect = true;
@@ -100,25 +124,38 @@ public class applicationFunctions implements ActionListener {
                 }
             }
 
+            //This is the if statement that will write all the data the user has entered into the data.txt file.
             if (isInformationCorrect == true) {
                 JOptionPane.showMessageDialog(null, "Success!", "Data Entered", 1);
+                //Adds one to databaseIDNum and displays it to the window because this is the unique ID of all entries.
                 databaseBuild.databaseIDNum++;
                 applicationBuild.lblDatabaseIDNum.setText(Integer.toString(databaseBuild.databaseIDNum));
+                //There is now officially at least one entry now that the information is correct.
                 databaseBuild.isThereOneEntry = true;
 
+                //The two next lines converts the first letter of the package name text that the user has entered to upper case.
+                //We did this for two reasons:
+                //1. It looks a little more professional in the databaseBuild table
+                //2. When sorting the package names the sort function would sort the uppercase letters before the lower case letters
+                //meaning that "X" would be sorted before "a" making the sorting a little off.
                 String packageName = applicationBuild.txtPackageName.getText();
                 String formattedPackageName = packageName.substring(0, 1).toUpperCase() + packageName.substring(1);
+
+                //This try catch statement puts the databaseID into a text file so that the databaseID is saved when the user reopens the program.
                 try {
+                    //The last parameter of the bufferedWriter decided whether or not we want to append the text to the text file.
                     BufferedWriter out = new BufferedWriter(new FileWriter(databaseIDTracker, false));
                     out.write(Integer.toString(databaseBuild.databaseIDNum));
                     out.close();
                 } catch (IOException error) {
-                    System.out.println("Could not create file");
+                    System.out.println("Something went wrong with id.txt");
                 }
 
+                //This try catch statement adds all the pieces of information entered by the user into the data.txt file.
                 try {
                     String[] informationTypes = {formattedPackageID, Integer.toString(databaseBuild.databaseIDNum), formattedPackageName, applicationBuild.formattedDate, applicationBuild.sm.getValue().toString()};
 
+                    //Append is true in this circumstance because we want the bufferedWriter to write after the last text file that has been entered.
                     BufferedWriter out = new BufferedWriter(new FileWriter(dataFile, true));
                     for (int i = 0; i < informationTypes.length; i++) {
                         out.write(informationTypes[i] + "<>");
@@ -126,7 +163,7 @@ public class applicationFunctions implements ActionListener {
                     out.newLine();
                     out.close();
                 } catch (IOException error) {
-                    System.out.println("Could not create file");
+                    System.out.println("Something went wrong with the data.txt file");
                 }
             }
         }
@@ -141,11 +178,10 @@ public class applicationFunctions implements ActionListener {
             applicationBuild.txtPackageID.requestFocus();
         }
 
-        // if the user presses the delete button run this code.
+        //DELETE BUTTON
         if (a.getSource() == applicationBuild.btnDelete) {
             // asks the user to enter the ID of the package they would like to remove from the database(data.txt).
-            String deleteID = JOptionPane.showInputDialog(null, "Enter the ID of the package you would like to delete",
-                    "Delete Package", 3);
+            String deleteID = JOptionPane.showInputDialog(null, "Enter the ID of the package you would like to delete", "Delete Package", 3);
             // this if statement is just to prevent an error if the user were to press either cancel or the close button. When the user presses cancel
             // or the close button without typing anything, deleteID seemed to equal null and caused an error when the next if statement tried to run.
             if (deleteID != null) {
@@ -191,6 +227,9 @@ public class applicationFunctions implements ActionListener {
                     } catch (IOException error) {
                         System.out.print("Error with deleting.");
                     }
+
+                    //This try catch statement rewrites the data from the temp.txt file to the main data.txt file which will be accessed the next time the user
+                    //hits clear.
                     try {
                         dataFile.delete();
                         dataFile = new File("src\\data.txt");
@@ -211,7 +250,7 @@ public class applicationFunctions implements ActionListener {
             }
         }
 
-        // if the user hits the login button at the beginning of the program run this code.
+        //LOGIN BUTTON
         if (a.getSource() == applicationLogin.btnLogin) {
             // this try statement will read through a txt file named id.txt that holds all the usernames and passwords of the people that
             // signed up for our package sorter.
@@ -247,13 +286,12 @@ public class applicationFunctions implements ActionListener {
             }
         }
 
-        // if the user presses the sign up button, create a new instance of the applicationSignUp class. This is where the user is able to sign up
-        // in order to gain access to the main program.
+        //SIGN UP BUTTON
         if (a.getSource() == applicationLogin.btnSignUp) {
             new applicationSignUp();
         }
 
-        // if the user attempts to sign up run this code.
+        //SIGN UP BUTTON INSIDE APPLICATIONSIGNUP.JAVA
         if (a.getSource() == applicationSignUp.btnSignUp) {
             // regex to determine if an email and password is valid.
             String userPattern = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
